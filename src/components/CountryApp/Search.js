d4c4e17d924cc55e8b4f0c677f1213f167e43e01
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-02-03 08:35:59 +0600

    search modified

--- src/components/CountryApp/Search.js
+++ src/components/CountryApp/Search.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import style from "./search.module.css";
 
-const Search = ({ onSearch }) => {   
+const Search = (props) => {
   const [searchText, setSearchText] = useState("");
 
   const handleChange = (e) => {
     setSearchText(e.target.value);
+    // alert(searchText);
   };
 
-  useEffect(() => {
-    onSearch(searchText);  
-  }, [searchText, onSearch]); 
+  useEffect(() => { 
+    props.onSearch(searchText);
+  }, [searchText]);
 
   return (
     <div style={{ textAlign: "center" }}>
-      <input type="text" placeholder="Search Country" value={searchText} onChange={handleChange}className={style["search-input"]}/>
+      <input type="text" placeholder="Search Country" value={searchText} onChange={handleChange} className={style["search-input"]}/>
     </div>
   );
 };
 
-export default Search;
+export default Search
